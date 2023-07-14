c003d4ffca20f67010486c3bc4a47c440f0ab2a1
Sanjay Garg <sgarg5858@gmail.com>
2023-07-14 18:58:58 +0530

    Basic Template Driven Implemented & Working

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-template-driven-form',
+  selector: 'template-form',
   templateUrl: './template-driven-form.component.html',
   standalone:true,
-  styles: [
-  ]
+  styleUrls:['./template-driven-form.component.scss'],
+  imports:[FormsModule,CommonModule]
 })
 export class TemplateDrivenFormComponent {
+
+  userInfo={
+    firstName:"Sanjay",
+    lastName:"Garg",
+    nickName:"Sanju",
+    email:"sgarg5858@gmail.com",
+    contact:"9041421558",
+    yearOfBirth:1998,
+    passport:"",
+    street:"Jawahar Nagar,Goniana Mandi",
+    city:"Bathinda",
+    postCode:151201,
+    password:"",
+    confirmPassword:""
+  }
+  isAdult=true;
+
+  onSubmit(form:NgForm,event:SubmitEvent)
+  {
+    console.log(form.value);
+  }
 
 }
