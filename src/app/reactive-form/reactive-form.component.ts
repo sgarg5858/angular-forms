391f9b4567023f2dd1b5156647d20e52dd0c7e24
Sanjay Garg <sgarg5858@gmail.com>
2023-07-15 18:41:30 +0530

    Reactive form in progress

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-reactive-form',
+  selector: 'reactive-form',
   templateUrl: './reactive-form.component.html',
   standalone:true,
-  styles: [
-  ]
+  styleUrls: ['./reactive-form.component.scss'],
+  imports:[ReactiveFormsModule,CommonModule],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class ReactiveFormComponent {
+
+  userForm = new FormGroup({
+    firstName:new FormControl('Sanjay',[Validators.required,Validators.minLength(3)]),
+    lastName:new FormControl('Garg',[Validators.required]),
+    email:new FormControl('sgarg5858@gmail.com',[Validators.required,Validators.email]),
+    nickName:new FormControl('Sanju',
+    {
+      validators:[Validators.required,Validators.minLength(3)],
+      asyncValidators:[]
+    }
+    ),
+    yearOfBirth:new FormControl(1998,[Validators.required,Validators.max(2005)]),
+    passport:new FormControl('CTXXTXXXT'),
+
+    address: new FormGroup({
+      street:new FormControl('#10 street no 4, Jawahar Nagar',[Validators.required]),
+      city:new FormControl('Bathinda',[Validators.required]),
+      postCode:new FormControl('151201',Validators.required),
+    }),
+    password: new FormGroup({
+      password:new FormControl('',[Validators.required,Validators.minLength(6)]),
+      confirmPassword:new FormControl('')
+    },{validators:[]}),
+    
+  })
+
+  onSubmit()
+  {
+    console.log(this.userForm);
+  }
+
 
 }
