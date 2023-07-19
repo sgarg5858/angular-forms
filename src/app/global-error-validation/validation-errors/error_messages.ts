44bc2ac69c02033e630af6f2e1e994c88d3e923f
Sanjay Garg <sgarg5858@gmail.com>
2023-07-19 11:24:02 +0530

    Configurable Error messages

--- src/app/global-error-validation/validation-errors/error_messages.ts
+++ src/app/global-error-validation/validation-errors/error_messages.ts
@@ -1,18 +1,24 @@
 import { InjectionToken } from "@angular/core"
 
-export const ERROR_MESSAGES: { [key: string]: string } = {
-    required: `This field is required`,
-    requiredTrue: `This field is required`,
-    email: `It should be a valid email`,
-    minlength: `The value length is too short`,
-    max: `The value  is too big`,
-    banWords: `This word isn't allowed`,
-    appBanWords: `This word isn't allowed`,
-    appPasswordShouldMatch: `Password should match`,
-    passwordShouldMatch: `Password should match`,
-    pattern: `Wrong format`,
-    appUniqueNickName: `Nickname is taken`,
-    uniqueName: `Nickname is taken`,
+export const ERROR_MESSAGES: { [key: string]: (errors:any)=> string } = {
+    required: () => `This field is required`,
+    requiredTrue: ()=> `This field is required`,
+    email: ()=> `It should be a valid email`,
+    minlength:(errors)=>{
+      console.log(errors);
+      return `The length should be atleast ${errors.requiredLength} characters`
+    },
+    max:(errors) => {
+      console.log(errors);
+      return `The length should be atleast ${errors.requiredLength} characters`
+    },
+    banWords:(errors)=> `This word isn't allowed`,
+    appBanWords:(errors)=> `This word isn't allowed`,
+    appPasswordShouldMatch: (errors)=> `Password should match`,
+    passwordShouldMatch: (errors)=> `Password should match`,
+    pattern: (errors)=> `Wrong format`,
+    appUniqueNickName: (errors)=> `Nickname is taken`,
+    uniqueName: (errors)=> `Nickname is taken`,
   }
 
   export const VALIDATION_ERROR_MESSAGES = new InjectionToken('Validation Error Messages',{
